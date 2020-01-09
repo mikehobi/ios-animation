feb6dea9f291b2e1a22ec95a2bc9f5a5bb43d10d
Mike Hobizal <mikehobizal@gmail.com>
2020-01-09 14:17:38 -0800

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Mike Hobizal on 1/9/20.
+//  Copyright © 2020 Mike Hobizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
