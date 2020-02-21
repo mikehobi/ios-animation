67c2a0c493a76b4531fe8064326abef8ba37e6f3
Mike Hobizal <mikehobizal@gmail.com>
2020-02-21 13:22:53 -0800

    update

---
+++ Animation/Animation.swift
@@ -0,0 +1,345 @@
+//
+// Animation.swift
+// 2020 Instrument Marketing
+//
+
+import UIKit
+
+/// Animation Executable
+typealias AnimationBlock = () -> Void
+
+/// Animation Protocol
+protocol AnimationProtocol {}
+
+/// Animation
+struct Animation: AnimationProtocol {
+    var animation: AnimationBlock
+    var duration: TimeInterval
+    var delay: TimeInterval?
+    var easing: CAMediaTimingFunction
+
+    init(
+        _ animation: @escaping AnimationBlock,
+        duration: TimeInterval,
+        delay: TimeInterval? = nil,
+        easing: CAMediaTimingFunction = .easeInOutSine) {
+        self.animation = animation
+        self.duration = duration
+        self.delay = delay
+        self.easing = easing
+    }
+
+    public func start(completion: ((Bool) -> Void)? = nil) {
+        CATransaction.begin()
+        CATransaction.setAnimationTimingFunction(easing)
+        UIView.animate(
+            withDuration: duration,
+            delay: delay ?? 0,
+            animations: animation,
+            completion: completion)
+        CATransaction.commit()
+    }
+
+    typealias Sequence = AnimationSequence
+    typealias Stagger = AnimationStagger
+    typealias Parallel = AnimationParallel
+    typealias Wait = AnimationWait
+}
+
+/// Animation Wait
+struct AnimationWait: AnimationProtocol {
+    var duration: TimeInterval
+
+    init(_ duration: TimeInterval) {
+        self.duration = duration
+    }
+}
+
+/// Animation Sequence
+struct AnimationSequence: AnimationProtocol {
+    public var animations: [AnimationProtocol]
+    var delay: TimeInterval
+    var interval: TimeInterval
+
+    init(
+        _ animations: [AnimationProtocol],
+        interval: TimeInterval = 0,
+        delay: TimeInterval = 0) {
+        self.animations = animations
+        self.delay = delay
+        self.interval = interval
+    }
+
+    public func start(completion: ((Bool) -> Void)? = nil) {
+        AnimationSequence.sequence(animations, startDelay: delay) {
+            completion?(true)
+        }
+    }
+
+    mutating func append(_ newElement: AnimationProtocol) {
+        animations.append(newElement)
+    }
+
+    mutating func insert(_ newElement: AnimationProtocol, at index: Int) {
+        animations.insert(newElement, at: index)
+    }
+
+    static func sequence(
+        _ blocks: [AnimationProtocol],
+        startDelay: TimeInterval = 0,
+        completion: (() -> Void)? = nil) {
+        func iterate(index: Int) {
+            guard let block = blocks.get(at: index) else {
+                return
+            }
+            switch block {
+            case is Animation:
+                guard let block = block as? Animation else { return }
+                CATransaction.begin()
+                CATransaction.setAnimationTimingFunction(block.easing)
+                UIView.animate(
+                    withDuration: block.duration,
+                    delay: block.delay ?? 0,
+                    animations: block.animation,
+                    completion: { _ in
+                        if index == blocks.count - 1 {
+                            completion?()
+                            return
+                        }
+                        iterate(index: index + 1)
+
+                })
+                CATransaction.commit()
+            case is Animation.Stagger:
+                guard let block = block as? Animation.Stagger else { return }
+                block.start { _ in
+                    if index == blocks.count - 1 {
+                        completion?()
+                        return
+                    }
+                    iterate(index: index + 1)
+                }
+            case is Animation.Sequence:
+                guard let block = block as? Animation.Sequence else { return }
+                block.start { _ in
+                    if index == blocks.count - 1 {
+                        completion?()
+                        return
+                    }
+                    iterate(index: index + 1)
+                }
+            case is Animation.Parallel:
+                guard let block = block as? Animation.Parallel else { return }
+                block.start { _ in
+                    if index == blocks.count - 1 {
+                        completion?()
+                        return
+                    }
+                    iterate(index: index + 1)
+                }
+            case is Animation.Wait:
+                guard let block = block as? Animation.Wait else { return }
+                DispatchQueue.main.asyncAfter(deadline: .now() + block.duration) {
+                    if index == blocks.count - 1 {
+                        completion?()
+                        return
+                    }
+                    iterate(index: index + 1)
+                }
+            default:
+                break
+            }
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + startDelay) {
+            iterate(index: 0)
+        }
+    }
+}
+
+/// Animation Stagger
+struct AnimationStagger: AnimationProtocol {
+    var animations: [AnimationProtocol]
+    var interval: TimeInterval
+    var delay: TimeInterval?
+
+    init(
+        _ animations: [AnimationProtocol],
+        interval: TimeInterval = 0,
+        delay: TimeInterval? = nil) {
+        self.animations = animations
+        self.interval = interval
+        self.delay = delay
+    }
+
+    public func start(completion: ((Bool) -> Void)? = nil) {
+        AnimationStagger.stagger(animations, interval: interval) {
+            completion?(true)
+        }
+    }
+
+    mutating func append(_ newElement: AnimationProtocol) {
+        animations.append(newElement)
+    }
+
+    mutating func insert(_ newElement: AnimationProtocol, at index: Int) {
+        animations.insert(newElement, at: index)
+    }
+
+    static func stagger(
+        _ blocks: [AnimationProtocol],
+        interval: TimeInterval,
+        startDelay: TimeInterval = 0,
+        completion: (() -> Void)? = nil) {
+        func iterate(index: Int, closure: ((Bool) -> Void)? = nil) {
+            guard let block = blocks.get(at: index) else {
+                return
+            }
+            switch block {
+            case is Animation:
+                guard let block = block as? Animation else { return }
+                CATransaction.begin()
+                CATransaction.setAnimationTimingFunction(block.easing)
+                UIView.animate(
+                    withDuration: block.duration,
+                    delay: block.delay ?? 0,
+                    animations: block.animation,
+                    completion: closure)
+                CATransaction.commit()
+            case is Animation.Stagger:
+                guard let block = block as? Animation.Stagger else { return }
+                block.start(completion: closure)
+            case is Animation.Sequence:
+                guard let block = block as? Animation.Sequence else { return }
+                block.start(completion: closure)
+            case is Animation.Parallel:
+                guard let block = block as? Animation.Parallel else { return }
+                block.start(completion: closure)
+            case is Animation.Wait:
+                guard let block = block as? Animation.Wait else { return }
+                DispatchQueue.main.asyncAfter(deadline: .now() + block.duration) {
+                    closure?(true)
+                }
+            default:
+                break
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+                let nmxtIndex = index + 1
+                iterate(index: index + 1) { _ in
+                    if nmxtIndex == blocks.count - 1 {
+                        completion?()
+                    }
+                }
+            }
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + startDelay) {
+            iterate(index: 0)
+        }
+    }
+}
+
+struct AnimationParallel: AnimationProtocol {
+    var animations: [AnimationProtocol]
+    var delay: TimeInterval
+
+    init(
+        _ animations: [AnimationProtocol],
+        delay: TimeInterval = 0) {
+        self.animations = animations
+        self.delay = delay
+    }
+
+    public func start(completion: ((Bool) -> Void)? = nil) {
+        AnimationParallel.parallel(animations, startDelay: delay) {
+            completion?(true)
+        }
+    }
+
+    mutating func append(_ newElement: AnimationProtocol) {
+        animations.append(newElement)
+    }
+
+    mutating func insert(_ newElement: AnimationProtocol, at index: Int) {
+        animations.insert(newElement, at: index)
+    }
+
+    static func parallel(
+        _ blocks: [AnimationProtocol],
+        startDelay: TimeInterval = 0,
+        completion: (() -> Void)? = nil) {
+        func iterate(index: Int, closure: ((Bool) -> Void)? = nil) {
+            guard let block = blocks.get(at: index) else {
+                return
+            }
+            switch block {
+            case is Animation:
+                guard let block = block as? Animation else { return }
+                CATransaction.begin()
+                CATransaction.setAnimationTimingFunction(block.easing)
+                UIView.animate(
+                    withDuration: block.duration,
+                    delay: block.delay ?? 0,
+                    animations: block.animation,
+                    completion: closure)
+                CATransaction.commit()
+            case is Animation.Stagger:
+                guard let block = block as? Animation.Stagger else { return }
+                block.start(completion: closure)
+            case is Animation.Sequence:
+                guard let block = block as? Animation.Sequence else { return }
+                block.start(completion: closure)
+            case is Animation.Parallel:
+                guard let block = block as? Animation.Stagger else { return }
+                block.start(completion: closure)
+            default:
+                break
+            }
+            let nmxtIndex = index + 1
+            iterate(index: index + 1) { _ in
+                if nmxtIndex == blocks.count - 1 {
+                    completion?()
+                }
+            }
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + startDelay) {
+            iterate(index: 0)
+        }
+    }
+}
+
+extension Collection {
+    func get(at index: Index) -> Iterator.Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
+extension CAMediaTimingFunction {
+    /// CSS default ease
+    static let ease = CAMediaTimingFunction(controlPoints: 0.17, 0.67, 0.83, 0.67)
+    static let easeInSine = CAMediaTimingFunction(controlPoints: 0.47, 0, 0.745, 0.715)
+    static let easeOutSine = CAMediaTimingFunction(controlPoints: 0.39, 0.575, 0.565, 1)
+    static let easeInOutSine = CAMediaTimingFunction(controlPoints: 0.445, 0.05, 0.55, 0.95)
+    static let easeInQuad = CAMediaTimingFunction(controlPoints: 0.55, 0.085, 0.68, 0.53)
+    static let easeOutQuad = CAMediaTimingFunction(controlPoints: 0.25, 0.46, 0.45, 0.94)
+    static let easeInOutQuad = CAMediaTimingFunction(controlPoints: 0.455, 0.03, 0.515, 0.955)
+    static let easeInCubic = CAMediaTimingFunction(controlPoints: 0.55, 0.055, 0.675, 0.19)
+    static let easeOutCubic = CAMediaTimingFunction(controlPoints: 0.215, 0.61, 0.355, 1)
+    static let easeInOutCubic = CAMediaTimingFunction(controlPoints: 0.645, 0.045, 0.355, 1)
+    static let easeInQuart = CAMediaTimingFunction(controlPoints: 0.895, 0.03, 0.685, 0.22)
+    static let easeOutQuart = CAMediaTimingFunction(controlPoints: 0.165, 0.84, 0.44, 1)
+    static let easeInOutQuart = CAMediaTimingFunction(controlPoints: 0.77, 0, 0.175, 1)
+    static let easeInQuint = CAMediaTimingFunction(controlPoints: 0.755, 0.05, 0.855, 0.06)
+    static let easeOutQuint = CAMediaTimingFunction(controlPoints: 0.23, 1, 0.32, 1)
+    static let easeInOutQuint = CAMediaTimingFunction(controlPoints: 0.86, 0, 0.07, 1)
+    static let easeInExpo = CAMediaTimingFunction(controlPoints: 0.95, 0.05, 0.795, 0.035)
+    static let easeOutExpo = CAMediaTimingFunction(controlPoints: 0.19, 1, 0.22, 1)
+    static let easeInOutExpo = CAMediaTimingFunction(controlPoints: 1, 0, 0, 1)
+    static let easeInCirc = CAMediaTimingFunction(controlPoints: 0.6, 0.04, 0.98, 0.335)
+    static let easeOutCirc = CAMediaTimingFunction(controlPoints: 0.075, 0.82, 0.165, 1)
+    static let easeInOutCirc = CAMediaTimingFunction(controlPoints: 0.785, 0.135, 0.15, 0.86)
+    static let easeInBack = CAMediaTimingFunction(controlPoints: 0.6, -0.28, 0.735, 0.045)
+    static let easeOutBack = CAMediaTimingFunction(controlPoints: 0.175, 0.885, 0.32, 1.275)
+    static let easeInOutBack = CAMediaTimingFunction(controlPoints: 0.68, -0.55, 0.265, 1.55)
+}
--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -12,9 +12,104 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let redBox1 = UIView()
+        redBox1.backgroundColor = .red
+
+        let redBox2 = UIView()
+        redBox2.backgroundColor = .red
+
+        let redBox3 = UIView()
+        redBox3.backgroundColor = .red
+
+        let stack = UIStackView(arrangedSubviews: [redBox1, redBox2, redBox3])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+
+        view.addSubview(stack)
+        stack.frame = CGRect(x: 0, y: 0, width: 100, height: 500)
+
+
+
+        let blueBox1 = UIView()
+        blueBox1.backgroundColor = .blue
+
+        let blueBox2 = UIView()
+        blueBox2.backgroundColor = .blue
+
+        let blueBox3 = UIView()
+        blueBox3.backgroundColor = .blue
+
+        let blueStack = UIStackView(arrangedSubviews: [blueBox1, blueBox2, blueBox3])
+        blueStack.axis = .vertical
+        blueStack.distribution = .fillEqually
+
+        view.addSubview(blueStack)
+        blueStack.frame = CGRect(x: 240, y: 0, width: 100, height: 500)
+
+        let anim1 = AnimationSequence([
+            Animation({ redBox1.alpha = 0.5 }, duration: 1.0),
+            Animation({ redBox2.alpha = 0.5 }, duration: 1.0),
+            Animation({ redBox3.alpha = 0.5 }, duration: 1.0),
+        ], interval: 5.0)
+
+        let anim2 = AnimationStagger([
+            Animation({ blueBox1.alpha = 0.5 }, duration: 1.0),
+            Animation({ blueBox2.alpha = 0.5 }, duration: 1.0),
+            Animation({ blueBox3.alpha = 0.5 }, duration: 1.0),
+        ], interval: 0.2)
+
+        let anim3 = AnimationStagger([
+            AnimationSequence([
+                Animation({ redBox1.alpha = 1.0 }, duration: 1.0),
+                Animation({ redBox1.alpha = 0.1 }, duration: 1.0),
+                Animation({ redBox1.alpha = 1.0 }, duration: 1.0),
+            ]),
+            AnimationSequence([
+                Animation({ redBox2.alpha = 1.0 }, duration: 1.0),
+                Animation({ redBox2.alpha = 0.1 }, duration: 1.0),
+                Animation({ redBox2.alpha = 1.0 }, duration: 1.0),
+            ]),
+            AnimationSequence([
+                Animation({ redBox3.alpha = 1.0 }, duration: 1.0),
+                Animation({ redBox3.alpha = 0.1 }, duration: 1.0),
+                Animation({ redBox3.alpha = 1.0 }, duration: 1.0),
+            ])
+        ], interval: 0.5)
+
+        let anim4 = AnimationSequence([
+            AnimationStagger([
+                Animation({ redBox1.alpha = 0.1 }, duration: 1.0),
+                Animation({ redBox2.alpha = 0.1 }, duration: 1.0),
+                Animation({ redBox3.alpha = 0.1 }, duration: 1.0),
+                Animation({ blueBox1.alpha = 0.1 }, duration: 1.0),
+                Animation({ blueBox2.alpha = 0.1 }, duration: 1.0),
+                Animation({ blueBox3.alpha = 0.1 }, duration: 1.0),
+            ], interval: 0.4),
+            AnimationStagger([
+                AnimationStagger([
+                    Animation({ redBox1.alpha = 1.0 }, duration: 1.0),
+                    Animation({ redBox2.alpha = 1.0 }, duration: 1.0),
+                    Animation({ redBox3.alpha = 1.0 }, duration: 1.0),
+                    ], interval: 0.1),
+                AnimationStagger([
+                    Animation({ blueBox1.alpha = 1.0 }, duration: 1.0),
+                    Animation({ blueBox2.alpha = 1.0 }, duration: 1.0),
+                    Animation({ blueBox3.alpha = 1.0 }, duration: 1.0),
+                ], interval: 0.1),
+            ], interval: 0.1)
+        ])
+
+        let anim5 = AnimationParallel([
+            Animation({ redBox1.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+            Animation({ redBox2.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+            Animation({ redBox3.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+            Animation({ blueBox1.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+            Animation({ blueBox2.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+            Animation({ blueBox3.transform = CGAffineTransform(translationX: 40, y: 0) }, duration: 1, easing: .easeInOutSine),
+        ])
+
+        AnimationSequence([ anim5]).start()
     }
-
-
 }
 
